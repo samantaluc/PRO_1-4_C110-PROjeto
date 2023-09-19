3a88564ca10bd47960283f4da4965ab917a831b7
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-09-19 15:36:25 -0300

    Update roll-a-dice.py

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -12,41 +12,17 @@ while response == "y":
 
     # condições para verificar o valor numérico  
     if no == 1: 
-        print("[-----]") 
-        print("[     ]") 
-        print("[  0  ]") 
-        print("[     ]") 
-        print("[-----]") 
+     
     if no == 2: 
-        print("[-----]") 
-        print("[ 0   ]") 
-        print("[     ]") 
-        print("[   0 ]") 
-        print("[-----]") 
+      
     if no == 3: 
-        print("[-----]") 
-        print("[     ]") 
-        print("[0 0 0]") 
-        print("[     ]") 
-        print("[-----]") 
+     
     if no == 4: 
-        print("[-----]") 
-        print("[0   0]") 
-        print("[     ]") 
-        print("[0   0]") 
-        print("[-----]") 
+   
     if no == 5: 
-        print("[-----]") 
-        print("[0   0]") 
-        print("[  0  ]") 
-        print("[0   0]") 
-        print("[-----]") 
+       
     if no == 6: 
-        print("[-----]") 
-        print("[0 0 0]") 
-        print("[     ]") 
-        print("[0 0 0]") 
-        print("[-----]") 
+  
           
     # Peça para o usuário informar uma resposta      
     response=input("Pressione y para jogar novamente e n para sair:") 
